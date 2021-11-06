b36d48d96484cc9361a7cce3856663545bbfee1d
Daniel Antonio Lopez Delgado <danielopez07@gmail.com>
2021-11-05 22:54:10 -0300

    Handling event endpoint type 'deposit'

--- models/Event.php
+++ models/Event.php
@@ -6,7 +6,7 @@ class Event {
         $this->read_csv_file();
     }
 
-    public function read_csv_file() {
+    private function read_csv_file() {
         // read accounts cache from CSV file
         $csv_file = dirname(__FILE__) . '/../accounts.csv';
         $f        = fopen($csv_file, 'r');
@@ -21,11 +21,68 @@ class Event {
         fclose($f);
     }
 
+    private function write_csv_file() {
+        $csv_file = dirname(__FILE__) . '/../accounts.csv';
+
+        if (file_exists($csv_file)) {
+            unlink($csv_file);
+        }
+
+        $f = fopen($csv_file, 'w');
+        if ($f === false) {
+            die('Error opening the file ' . $csv_file);
+        }
+        foreach ($this->accounts as $row) {
+            fputcsv($f, $row);
+        }
+        fclose($f);
+    }
+
     public function deposit($input) {
-        return [
-            'status' => '200',
-            'data' => $input,
-        ];
+        if (!isset($input->destination) || !is_numeric($input->destination)) {
+            return [
+                'status' => '404',
+                'data' => 'Destination not found',
+            ];
+        }
+        if (!isset($input->amount) || !is_numeric($input->amount) || $input->amount < 0) {
+            return [
+                'status' => '400',
+                'data' => 'Amount not valid'
+            ];
+        }
+
+        $account_id         = $input->destination;
+        $account_ids_column = array_column($this->accounts, 0);
+        $account_index      = array_search($account_id, $account_ids_column);
+
+        if ($account_index === false) {
+            $this->accounts[] = [$input->destination, $input->amount];
+            $this->write_csv_file();
+            return [
+                'status' => '201',
+                'data'   => [
+                    'destination' => [
+                        'id'      => $input->destination,
+                        'balance' => $input->amount
+                    ]
+                ] ,
+            ];
+        } else {
+            $this->accounts[$account_index][1] = (float)$this->accounts[$account_index][1] + (float)$input->amount;
+
+            $this->write_csv_file();
+
+            return [
+                'status' => '201',
+                'data'   => [
+                    'destination' => [
+                        'id'      => $this->accounts[$account_index][0],
+                        'balance' => $this->accounts[$account_index][1]
+                    ]
+                ] ,
+            ];
+        }
     }
 
     public function withdraw($input) {
